9d20194176f26a18b58a2398859607750811b074
darthale23 <74798151+darthale23@users.noreply.github.com>
2021-11-03 09:49:13 -0400

    Add files via upload

--- speedyFingers.py
+++ speedyFingers.py
@@ -1,76 +1,85 @@
 import pygame
+import button
 from pygame.locals import *
 import sys
 import time
   
-class Game:
+#initiliaze pygame
+pygame.init()
 
-    def __init__(self):
-        self.w=1000
-        self.h=750
-        self.reset=True
-        self.active = False
-        self.end = False
-        self.HEAD_C = (255,213,102)
-        self.TEXT_C = (300,300,300)
+#glabl variables
+width = 1000
+height = 700
+screen  = pygame.display.set_mode((width,height))
+gameOn = True
 
-        pygame.init()
+background = pygame.image.load("intro.png")
+background = pygame.transform.scale(background, (width, height))
 
-        self.show_intro = pygame.image.load('intro.png')
-        self.show_intro = pygame.transform.scale(self.show_intro, (self.w,self.h))
-    
-        self.home = pygame.image.load('home.png')
-        self.home = pygame.transform.scale(self.home, (self.w,self.h))
+intro = pygame.image.load("intro.png")
+intro = pygame.transform.scale(intro, (width, height))
 
-        self.stage_1 = pygame.display.set_mode((self.w,self.h))
-        pygame.display.set_caption('Speedy Fingers')
+#button images
+arrow_img = pygame.image.load("arrow.png").convert_alpha()
+arrow_img = pygame.transform.scale(arrow_img, (290, 290))
 
-    #from tutorial    
-    def draw_text(self, screen, msg, y ,fsize, color):
+#play_img = pygame.image.load("play.png").convert_alpha()
+#play_img = pygame.image.load("play.png").convert_alpha()
 
-        font = pygame.font.Font(None, fsize)
-        text = font.render(msg, 1,(0,128,128))
-        text_rect = text.get_rect(center=(self.w/3.5, y))
-        screen.blit(text, text_rect)
-        pygame.display.update()   
+#rules_img = pygame.image.load("rules.png").convert_alpha()
+#rules_img = pygame.image.load("rules.png").convert_alpha()
 
 
-    def run(self):
-
-        #reset from top
-        self.reset_now()
-        #set status to true
-        self.running=True
-
-        #if game is running, look for events
-        while(self.running):
-            self.stage_1.fill((0,0,0), (50,250,650,50))
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    self.running = False
-                    sys.exit()
-
-    def reset_now(self):
-        #go to intro screen, maybe in future change to go home screen?
-        self.stage_1.blit(self.show_intro, (0,0))
-        pygame.display.update()
-
-        #wait 5 seconds on intro screen
-        time.sleep(3)
-
-        self.reset=False
-        self.end = False
 
 
-        #draw home screen
-        self.stage_1.fill((0,0,0))
-        self.stage_1.blit(self.home,(0,0))
-        #home message on main stage filled with background
-        msg = "Speedy"
-        msg2 = "Fingers"
-        self.draw_text(self.stage_1, msg,300, 80,self.HEAD_C)  
-        self.draw_text(self.stage_1, msg2,400, 80,self.HEAD_C) 
-        pygame.display.update()
+#button class
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
 
-        
-Game().run()
+    def draw(self):
+        action = False
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+
+        #draw button on screen
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
+
+#create button instances
+arrow_button = Button(700, 350, arrow_img)
+
+
+
+
+
+
+
+
+
+
+
+while gameOn:
+    #taking event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameOn = False
+        if arrow_button.draw():
+            print("ONTO THE NExT PAGEEE") 
+
+    screen.blit(background, (0,0))
+    arrow_button.draw()
+    pygame.display.update()
+
