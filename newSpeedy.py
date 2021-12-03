467e2b4afff731b86624833ac0c58a3f6132f0f3
sophie <spavia@me.com>
2021-12-03 14:04:31 -0500

    fixed percent calculation
    more accurate, rather than char by char off

--- newSpeedy.py
+++ newSpeedy.py
@@ -6,6 +6,7 @@ import time
 import DBsetup as DB #for DBsetup.py
 import random
 import sqlite3 as sql
+from difflib import SequenceMatcher #for percent calculation
 
 # main program
 def main():
@@ -238,14 +239,8 @@ def play(screen, w,h):
                             totalTime = time.time() - start
 
                             #determine percent chars correct
-                            count = 0
-                            for i, char in enumerate(word):
-                                try:
-                                    if user_input[i] == char:
-                                        count +=1
-                                except:
-                                    pass
-                            percent = count/len(word)*100
+                            percent = SequenceMatcher(None, user_input, word).ratio()
+                            percent = percent*100
 
                             #determine words typed per minute
                             wpm = len(user_input)*60/(5*totalTime)
